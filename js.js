5a680fcb006ba9adfba819687a167d38835cf4d2
Hayk Antonyan <hayka5367@gmail.com>
2023-12-19 00:45:14 +0400

    added html elements which I will use feature

--- js.js
+++ js.js
@@ -4,3 +4,7 @@ if(!localStorage.getItem("dataReplace")){
     localStorage.setItem("dataReplace", JSON.stringify(data))
 }
 let arr = JSON.parse(localStorage.getItem("dataReplace"));
+const tableElm = document.querySelector("tbody");
+const addButton = document.getElementById("add")
+const deleteButton = document.getElementById("delete")
+const textArea = document.querySelector("textarea")
