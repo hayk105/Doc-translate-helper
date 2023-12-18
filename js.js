d5f40da8897cf822d837140b308fdbc974236914
Hayk Antonyan <hayka5367@gmail.com>
2023-12-19 00:39:47 +0400

    the code in this part configures local Storage data

--- js.js
+++ js.js
@@ -1,1 +1,6 @@
 import data from "./api/defaultData.js";
+
+if(!localStorage.getItem("dataReplace")){
+    localStorage.setItem("dataReplace", JSON.stringify(data))
+}
+let arr = JSON.parse(localStorage.getItem("dataReplace"));
